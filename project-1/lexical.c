0a3bdce1d3973d176f478ddacf2b11a58002f529
keverino <kmlee1127@gmail.com>
2015-01-22 11:44:50 -0800

    zzzzzz

--- project-1/lexical.c
+++ project-1/lexical.c
@@ -17,20 +17,64 @@ void getNonBlank();
 int lex();
 
 //Character classes
-#define LETTER 0
-#define DIGIT 1
+#define LETTER 98
+#define DIGIT 97
 #define UNKNOWN 99
 
 //Token codes
+#define _boolean          1
+#define _break            2
+#define _class            3
+#define _double           4
+#define _else             5
+#define _extends          6
+#define _for              7
+#define _if               8
+#define _implements       9
+#define _int              10
+#define _interface        11
+#define _newarray         12
+#define _println          13
+#define _readln           14
+#define _return           15
+#define _string           16
+#define _void             17
+#define _while            18
+#define _plus             19
+#define _minus            20
+#define _multiplication   21
+#define _division         22
+#define _mod              23
+#define _less             24
+#define _lessequal        25
+#define _greater          26
+#define _greaterqual      27
+#define _equal            28
+#define _notequal         29
+#define _and              30
+#define _or               31
+#define _not              32
+#define _assignop         33
+#define _semicolon        34
+#define _comma            35
+#define _period           36
+#define _leftparen        37
+#define _rightparen       38
+#define _leftbracket      39
+#define _rightbracket     40
+#define _leftbrace        41
+#define _rightbrace       42
+#define _intconstant      43
+#define _doubleconstant   44
+#define _stringconstant   45
+#define _booleanconstant  46
+#define _id               47
+
+
+
 #define INT_LIT 10
 #define IDENT 11
 #define ASSIGN_OP 20
-#define ADD_OP 21
-#define SUB_OP 22
-#define MULT_OP 23
-#define DIV_OP 24
-#define LEFT_PAREN 25
-#define RIGHT_PAREN 26
 
 //----------------------------------------------------------------------------------------------------
 int main()
@@ -49,32 +93,32 @@ int lookup(char ch)
 	{
     case '(':
       addChar();
-      nextToken = LEFT_PAREN;
+      nextToken = _leftparen;
       break;
     
     case ')':
       addChar();
-      nextToken = RIGHT_PAREN;
+      nextToken = _rightparen;
       break;
 
     case '+':
       addChar();
-      nextToken = ADD_OP;
+      nextToken = _plus;
       break;
 
     case'-':
       addChar();
-      nextToken = SUB_OP;
+      nextToken = _minus;
       break;
 
     case'*':
       addChar();
-      nextToken = MULT_OP;
+      nextToken = _multiplication;
       break;
 
     case'/':
       addChar();
-      nextToken = DIV_OP;
+      nextToken = _division;
       break;
 
     default:
