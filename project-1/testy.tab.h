d7245ce1bf5703615c676abe6ae93bd22ba4c5ff
keverino <kmlee1127@gmail.com>
2015-01-29 18:33:25 -0800

    asdf

---
+++ project-1/testy.tab.h
@@ -0,0 +1,10 @@
+#define t_INT 1000
+#define t_FLOAT 1001
+#define t_ADDOP 1002
+#define t_MULOP 1003
+#define t_COMMA 1004
+#define t_SEMICOL 1005
+#define t_DOT 1006
+#define t_NUM 1007
+#define t_ID 1008
+int yylval;
